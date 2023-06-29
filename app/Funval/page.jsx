81e59f58d291e954b5da12f3d38893aaa6f9b43e
Carlos Alvarado <carlosjam10@gmail.com>
2023-06-29 14:07:52 -0500

    culminado

--- app/Funval/page.jsx
+++ app/Funval/page.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function page() {
   return (
     <>
-      <h1>Funval</h1>
+      <h1>Funval International</h1>
       <p>
         Seguimos cambiando vidas 22 años de experiencia ¡Feliz 22 aniversario,
         con el compromiso de mejorar la vida de las personas a través de la
-        educación y el empleo. ¡Que sigamos creciendo juntos por muchos años más carlos alvarado
+        educación y el empleo. ¡Que sigamos creciendo juntos por muchos años más carlos alvarado developer
       </p>
     </>
   );
