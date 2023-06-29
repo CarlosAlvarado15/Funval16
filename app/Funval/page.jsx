5b8a60ca6e564eb495b7c34a60870caa5798048b
Carlos Alvarado <carlosjam10@gmail.com>
2023-06-29 13:54:05 -0500

    listo

--- app/Funval/page.jsx
+++ app/Funval/page.jsx
@@ -7,7 +7,7 @@ function page() {
       <p>
         Seguimos cambiando vidas 22 años de experiencia ¡Feliz 22 aniversario,
         con el compromiso de mejorar la vida de las personas a través de la
-        educación y el empleo. ¡Que sigamos creciendo juntos por muchos años más
+        educación y el empleo. ¡Que sigamos creciendo juntos por muchos años más carlos alvarado
       </p>
     </>
   );
